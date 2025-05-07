f3e98dcd2e60d63c6dbb5a6cb5e088aa0a3f93f0
Muhammad Mubsher Saeed <m03004203270@gmail.com>
2025-05-07 18:05:22 +0500

    OperatingHours

--- controllers/clinicController.js
+++ controllers/clinicController.js
@@ -20,16 +20,18 @@ exports.getAllClinics = async (req, res) => {
   }
 };
 
-// Read one
-exports.getClinicById = async (req, res) => {
-  try {
-    const clinic = await Clinic.findById(req.params.id);
-    if (!clinic) return res.status(404).json({ message: 'Clinic not found' });
-    res.json(clinic);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+// // Read one
+// exports.getClinicById = async (req, res) => {
+//   try {
+//     const clinic = await Clinic.findById(req.params.id);
+//     if (!clinic) return res.status(404).json({ message: 'Clinic not found' });
+//     res.json(clinic);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// };
+
+
 
 // Update
 exports.updateClinic = async (req, res) => {
