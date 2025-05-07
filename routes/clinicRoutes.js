8a843093b4b3f72fed92cbc33c8db457fc4da4c1
Muhammad Mubsher Saeed <m03004203270@gmail.com>
2025-05-07 18:07:36 +0500

    OperatingHour update

--- routes/clinicRoutes.js
+++ routes/clinicRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const clinicController = require('../controllers/clinicController');
 
-// router.post('/', clinicController.createClinic);
-// router.get('/', clinicController.getAllClinics);
-// router.get('/:id', clinicController.getClinicById);
-// router.put('/:id', clinicController.updateClinic);
-// router.delete('/:id', clinicController.deleteClinic);
+router.post('/', clinicController.createClinic);
+router.get('/', clinicController.getAllClinics);
+router.get('/:id', clinicController.getClinicById);
+router.put('/:id', clinicController.updateClinic);
+router.delete('/:id', clinicController.deleteClinic);
 
 
 module.exports = router;
