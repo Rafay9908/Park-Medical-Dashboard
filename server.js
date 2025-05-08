a69fd31ae769ea6b44b28cfbe5dc2c39b829f404
Muhammad Mubsher Saeed <m03004203270@gmail.com>
2025-05-08 19:11:11 +0500

    server update

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
+// const express = require('express');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
+// const connectDB = require('./config/db');
+
+// const userRoutes = require('./routes/userRoutes');
+// const slotRoutes = require('./routes/slotRoutes');
+// const clinicRoutes = require('./routes/clinicRoutes');
+
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+// app.use(cors());
+
+// // ✅ If you want to restrict it to a specific origin (optional)
+// app.use(cors({
+//   origin: 'http://localhost:3000',  // change to your frontend domain
+//   credentials: true
+// }));
+// app.use(express.json());
+
+// app.use('/api/users', userRoutes);
+// app.use('/api/slots', slotRoutes);
+// app.use('/api/clinics', clinicRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -6,16 +35,15 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const slotRoutes = require('./routes/slotRoutes');
 const clinicRoutes = require('./routes/clinicRoutes');
-
+const clinicianRoutes = require('./routes/clinicianRoutes');
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(cors());
 
-// ✅ If you want to restrict it to a specific origin (optional)
 app.use(cors({
-  origin: 'http://localhost:3000',  // change to your frontend domain
+  origin: 'http://localhost:5173',  
   credentials: true
 }));
 app.use(express.json());
@@ -23,6 +51,7 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/slots', slotRoutes);
 app.use('/api/clinics', clinicRoutes);
+app.use('/api/clinicians', clinicianRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
