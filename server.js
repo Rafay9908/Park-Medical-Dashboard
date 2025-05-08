dea0846502b49fccc36d7484accef064a68a8085
Muhammad Mubsher Saeed <m03004203270@gmail.com>
2025-05-08 19:24:44 +0500

    server update ClinicianRoutes

--- server.js
+++ server.js
@@ -1,31 +1,4 @@
-// const express = require('express');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
 
-// const userRoutes = require('./routes/userRoutes');
-// const slotRoutes = require('./routes/slotRoutes');
-// const clinicRoutes = require('./routes/clinicRoutes');
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-// app.use(cors());
-
-// // ✅ If you want to restrict it to a specific origin (optional)
-// app.use(cors({
-//   origin: 'http://localhost:3000',  // change to your frontend domain
-//   credentials: true
-// }));
-// app.use(express.json());
-
-// app.use('/api/users', userRoutes);
-// app.use('/api/slots', slotRoutes);
-// app.use('/api/clinics', clinicRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const express = require('express');
 const cors = require('cors');
