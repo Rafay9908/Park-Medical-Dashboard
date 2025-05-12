fae5423fd2e724e0bcb4631609c17ce4d1fa1989
Muhammad Mubsher Saeed <m03004203270@gmail.com>
2025-05-12 16:07:35 +0500

    Test

--- server.js
+++ server.js
@@ -9,15 +9,10 @@ const userRoutes = require('./routes/userRoutes');
 const slotRoutes = require('./routes/slotRoutes');
 const clinicRoutes = require('./routes/clinicRoutes');
 const clinicianRoutes = require('./routes/clinicianRoutes');
-<<<<<<< HEAD
 // const rotaRoutes = require('./routes/rotaController')
 const rotaRoutes = require('./routes/rotaRoutes');
 
 
-=======
-const rotaRoutes = require('./routes/rotaRoutes');
-
->>>>>>> 803a6539f07d4c4e40b900efc2903953a3df7893
 dotenv.config();
 connectDB();
 
@@ -34,7 +29,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/slots', slotRoutes);
 app.use('/api/clinics', clinicRoutes);
 app.use('/api/clinicians', clinicianRoutes);
-<<<<<<< HEAD
 // app.use('/api/rota', rotaRoutes);
 =======
 >>>>>>> 803a6539f07d4c4e40b900efc2903953a3df7893
