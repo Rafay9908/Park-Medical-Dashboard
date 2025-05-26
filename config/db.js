94f4a1d1f2f1f77904d62b7f66cf94a7751cbb8a
Rafay9908 <abdulrafay9908@gmail.com>
2025-05-26 21:36:22 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URL || process.env.MONGODB_URI ||  'mongodb://127.0.0.1:27017/medical_clinic';
+    const mongoURI = process.env.MONGO_URL || process.env.MONGO_URI ||  'mongodb://127.0.0.1:27017/medical_clinic';
     
     
     
